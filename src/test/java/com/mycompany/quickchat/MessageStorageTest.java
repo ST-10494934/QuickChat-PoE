0fd6044060e2a4b0b1215a4fc3a08117ea7d0e89
ST10494934 <st10494934@myemeris.edu.za>
2026-06-25 01:12:56 +0200

    Created MessageStorageTest.java

---
+++ src/test/java/com/mycompany/quickchat/MessageStorageTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quickchat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for MessageStorage class - Part 3
+ *
+ * @author Lorenza
+ */
+public class MessageStorageTest {
+    
+    private MessageStorage storage;
+    
+    @BeforeEach
+    public void setUp() {
+        storage = new MessageStorage();
+        
+        
+    }
+    
+}
