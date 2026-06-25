8f708ad1c28fd9e8c6845857216d11db00f98ded
ST10494934 <st10494934@myemeris.edu.za>
2026-06-25 02:07:00 +0200

    Create MessageStorage unit test class and setup method

--- src/test/java/com/mycompany/quickchat/MessageStorageTest.java
+++ src/test/java/com/mycompany/quickchat/MessageStorageTest.java
@@ -20,7 +20,36 @@ public class MessageStorageTest {
     public void setUp() {
         storage = new MessageStorage();
         
+        // Test Data Message 1 - Sent 
+        storage.addSentMessage("1234567890", "12:1:DIDCAKE",
+                "+27834557896", "Did you get the cake?");
         
+        //Test Data Message 2 - Stored
+        storage.addStoredMessage ("2345678901", "23:2:WHERETIME"
+                "+27838884567",
+                "Where are you? You are late! I have asked you to be on time.");
+        
+        // Test Data Message 3 - Disregarded
+        storage.addDisregardedMessage("Yohoooo, I am at your gate.");
+
+        // Test Data Message 4 - Sent
+        storage.addSentMessage("3456789012", "34:4:ITTIME",
+                "0838884567", "It is dinner time !");
+
+        // Test Data Message 5 - Stored
+        storage.addStoredMessage("4567890123", "45:5:OKWITHOUT",
+                "+27838884567", "Ok, I am leaving without you.");
     }
     
+
+
+
+
+
+
+
+
+
+
+
 }
